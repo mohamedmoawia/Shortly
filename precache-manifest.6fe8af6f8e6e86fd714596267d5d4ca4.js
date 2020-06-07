820ab17c26d90d5dbb88e66091e5001ae2c14594
mohamedmoawia <mo3wia94@gmail.com>
2020-06-07 11:52:09 +0200

    Updates

---
+++ precache-manifest.6fe8af6f8e6e86fd714596267d5d4ca4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "604681137fbe48bdc80007bd48bdd739",
+    "url": "/Shortly/index.html"
+  },
+  {
+    "revision": "00132129ebf1e6af7d14",
+    "url": "/Shortly/static/css/main.c7cb2c67.chunk.css"
+  },
+  {
+    "revision": "222b971c360ff708e775",
+    "url": "/Shortly/static/js/2.92c83b61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Shortly/static/js/2.92c83b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00132129ebf1e6af7d14",
+    "url": "/Shortly/static/js/main.a6c82b63.chunk.js"
+  },
+  {
+    "revision": "0def47a3fd2e6105312a",
+    "url": "/Shortly/static/js/runtime-main.d56fa237.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/Shortly/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/Shortly/static/media/bg-shorten-desktop.a82de60e.svg"
+  }
+]);
